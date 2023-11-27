d1e68b0bf16fedb34aa4d582acbaff84f80105fd
ValerioTorto <valerio.tortorelli@gmail.com>
2023-11-27 11:03:48 +0100

    first part

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
-let basePriceBack = 20.50;
-let basePriceFront = 15.30;
-let baseAnal = 33.60;
-let hoursRequ = prompt("ore da lavorare");
-parseInt(hoursRequ);
 
-let totalPriceBack = (basePriceBack * hoursRequ);
-console.log(totalPriceBack)
 
 function previewServ(event) {
     event.preventDefault();
     let serviceType = document.getElementById("serviceType");
-    let serviceOpt = serviceType.options[categoryType.selectedIndex].value;
+    let serviceOpt = serviceType.options[serviceType.selectedIndex].value;
 
-    
+    let hoursRequ = document.getElementById("hours").value;
+    parseInt(hoursRequ);
+     if(serviceOpt == "Backend") {
+        let price = hoursRequ * 20.50
+        console.log(price)
+     }
+     else if (serviceOpt == "Frontend") {
+        let price = hoursRequ * 15.30
+        console.log(price)
+     }
+     else if (serviceOpt == "Projectanalysis") {
+        let price = hoursRequ * 33.60
+        console.log(price)
+     }
+
 
 }
