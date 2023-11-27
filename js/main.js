7f094f14b5e2a6158af3f6dc24e9f1deb1bbd574
ValerioTorto <valerio.tortorelli@gmail.com>
2023-11-27 10:35:58 +0100

    Base

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 let basePriceBack = 20.50;
-let 
+let basePriceFront = 15.30;
+let baseAnal = 33.60;
+let hoursRequ = prompt("ore da lavorare");
+parseInt(hoursRequ);
+
+let totalPriceBack = (basePriceBack * hoursRequ);
+console.log(totalPriceBack)
+
+function previewServ(event) {
+    event.preventDefault();
+    let serviceType = document.getElementById("serviceType");
+    let serviceOpt = serviceType.options[categoryType.selectedIndex].value;
+
+    
+
+}
